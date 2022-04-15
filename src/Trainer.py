dafe967733b43916c0d119c0f318ab6a0f9534e9
MK8BK <64875519+MK8BK@users.noreply.github.com>
2022-04-15 07:24:32 +0200

    started math stuff

--- src/Trainer.py
+++ src/Trainer.py
@@ -5,7 +5,22 @@ from src.NeuralNetwork import *
 
 def train_on_subset(net, path_str, epochs, batch_size, equilibrium=False,
 			lr=0.01, test_on_all=False, iter_test=20):
-	
+	"""
+		The main training funtion
+		@param: net : a NeuralNetwork object from src.NeuralNetwork
+		@param: path_str : a string path to the data set folder
+		@param: epochs : the number of training iterations
+		@param: batch_size : the number of images per training
+		@param: equilibrium : optional bool=False : divide batch_size equally per class
+		@param:	lr : optional float=0.01 : learning rate
+		@param: test_on_all : optional bool=False : test on whole data set (38400)
+		@param: iter_test : optional int=20 : test every iter_test iterations, print
+		@return: accuracies: list[float] : ordered percentages of testing (float 0-100.)
+		@return: costs : list[float] : ordered costs of testing 
+			len(accuracies)=len(costs)+1 (accuracies initialised with 0)
+
+	"""
+
 	accuracies = [0]
 	costs = []
 	length_of_string = len(str(epochs))
